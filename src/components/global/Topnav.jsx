50a8b66ab8009ae3a466c6d9cbb9a631ad87aa58
Elijahwb <brandonelijah099@gmail.com>
2021-12-26 05:45:20 +0300

    worked on the scroll bar of the pages

--- src/components/global/Topnav.jsx
+++ src/components/global/Topnav.jsx
@@ -9,9 +9,9 @@ const Topnav = () => {
                 <img src={logo} className='object-cover' style={{height: '90px'}} alt="uia-logo" />
             </Link>
             <div className='flex items-end mr-2'>
-                <Link className='mr-4 font-semibold text-gray-600 hover:text-blue-500' to='#'>F.A.Q.</Link>
-                <button className='mr-4 outline-none rounded text-sm py-1 px-4 bg-gradient-to-b from-red-500 to-red-900 text-white hover:opacity-90'>Log In</button>
-                <button className='outline-none rounded text-sm py-1 px-4 bg-gradient-to-b from-red-500 to-red-900 text-white hover:opacity-90'>Sign Up</button>
+                <Link className='mr-4 font-semibold text-gray-600 hover:text-lime-500' to='#'>F.A.Q.</Link>
+                <Link to='/' className='mr-4 outline-none rounded text-sm py-1 px-4 bg-gradient-to-b from-red-500 to-red-900 text-white hover:opacity-90'>Log In</Link>
+                <Link to='/register' className='outline-none rounded text-sm py-1 px-4 bg-gradient-to-b from-red-500 to-red-900 text-white hover:opacity-90'>Sign Up</Link>
             </div>
         </section>
     );
